a3addb2c4c4d1c32066b3ba0eafb9a67010a7e25
Taylor <tjaroguetech@gmail.com>
2021-01-05 10:24:10 -0500

    Added in Goal frequency inclusion validations. Testing some seed data

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,5 @@
 class Goal < ApplicationRecord
     belongs_to :user
-    validates :title, :frequency, :is_complete, :start_date, :end_date, presence: true
+    validates :title, :frequency, :start_date, :end_date, presence: true
+    validates_inclusion_of :frequency, in => ['daily', 'weekly', 'bi-weekly', 'monthly', 'semi-annually', 'annually']
 end
